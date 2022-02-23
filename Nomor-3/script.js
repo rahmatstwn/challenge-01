999232df33e17bc8acd2ef401ac5788ca47f9236
rahmatstwn <rahmat.118140097@student.itera.ac.id>
2022-02-23 20:38:03 +0700

    Nomor 3

---
+++ Nomor-3/script.js
@@ -0,0 +1,25 @@
+const checkEmail = (email) => {
+    const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if(typeof email === 'string' && email.includes('@')){
+        if(email.match(regexEmail)){
+            return "VALID"
+        }else{
+            return "INVALID"
+        }
+    }else{
+        if (email === undefined){
+            return "Email tidak terisi"
+        }
+        else {
+            return "Email must included"
+        }
+    }
+}
+
+console.log(checkEmail("rahmat@gmail.co.id"))
+console.log(checkEmail("rahmat@gmail.com"))
+console.log(checkEmail("rahmat"))
+console.log(checkEmail("rahmat@gmail"))
+console.log(checkEmail("1234"))
+console.log(checkEmail())
